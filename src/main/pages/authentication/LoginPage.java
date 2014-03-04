2c5e9f77af00a285e8e9e7d8f4387cd18dcd475e
Brian Smith <crazycabo350@gmail.com>
2014-03-04 16:19:47 -0500

    Added additional elements

--- src/main/pages/authentication/LoginPage.java
+++ src/main/pages/authentication/LoginPage.java
@@ -11,89 +11,88 @@ import org.openqa.selenium.support.ui.LoadableComponent;
 import static junit.framework.Assert.assertTrue;
 
 /**
- * Created By: bsmith on 2/24/14.
+ * Created By: Brian Smith on 2/24/14.
  * Package: authentication
- * Description: This is a template of a fictitious page object representing the first page displayed when navigating
- * to an application. This class utilizes loadable components.
+ * Description:
  */
 public class LoginPage extends LoadableComponent<LoginPage>{
-    //The local object of the driver passed in to the constructor.
     private WebDriver localDriver;
 
-    //Examples of several ways to find elements using PageFactory.
-    @FindBy(how= How.NAME, using="user")
-    @CacheLookup //Indicates the element will not be looked up constantly in the DOM.
-    public WebElement userName;
-
-    @FindBy(how= How.NAME, using="user-error")
-    public WebElement userNameErrorMessage;
-
-    @FindBy(how= How.ID, using="password")
+    @FindBy(how= How.CSS, using=".email-field input")
     @CacheLookup
-    public WebElement password;
+    public WebElement emailInput;
 
-    @FindBy(how= How.ID, using="password-error")
-    public WebElement passwordErrorMessage;
+    @FindBy(how= How.CSS, using=".email-field small:last-child")
+    public WebElement emailInputError;
 
-    @FindBy(how= How.XPATH, using="//input[@name='submit']")
+    @FindBy(how= How.CSS, using=".password-field input")
     @CacheLookup
+    public WebElement passwordInput;
+
+    @FindBy(how= How.CSS, using=".password-field small:last-child")
+    public WebElement passwordInputError;
+
+    @FindBy(how= How.CSS, using="form input:last-child")
+    public WebElement rememberComputer;
+
+    @FindBy(how= How.CSS, using="form button:first-child")
     public WebElement submitButton;
 
-    @FindBy(how= How.CSS, using="")
-    @CacheLookup
-    public WebElement nextButton;
+    @FindBy(how= How.CSS, using="form button:last-child")
+    public WebElement createAccountButton;
 
-    //Constructor accepting the driver only as it is the root page in the application structure.
+    @FindBy(how= How.CSS, using="form .row:last-child div")
+    public WebElement version;
+
     public LoginPage(WebDriver driver) {
         this.localDriver = driver;
-        //Initialize all elements found in the above examples.
         PageFactory.initElements(this.localDriver, this);
     }
 
     /**
-     * Method attempts to load the page if isLoaded() throws an error
-     * when an object of this class is initially created.
+     * Load the login page.
      */
     @Override
     protected void load() {
-        localDriver.navigate().to("http://www.yoursite.com");
+        localDriver.manage().deleteAllCookies();
+        localDriver.navigate().to("http://localhost:3000");
     }
 
     /**
-     * Method initially called when using the get() method on the object. If an error is thrown, the load()
-     * method is called. isLoaded() is called again once loaded and a second failure will cause an error.
+     * Throw an error if the login page cannot load.
      * @throws Error
      */
     @Override
     protected void isLoaded() throws Error {
         //Assert if the title does not match.
-        assertTrue("The login page does not display.", localDriver.getTitle().equals("Title"));
+        String title = localDriver.getTitle();
+        assertTrue("The login page does not display.", localDriver.getTitle().equals("MockDriver - Login"));
     }
 
     /**
      * Enter your account credentials when attempting to log into the application.
-     * @param userName your account user name
+     * @param email your account email address
      * @param password your account password
      */
-    public void enterUserNameAndPassword(String userName, String password) {
-        this.userName.sendKeys(userName);
-        this.password.sendKeys(password);
+    public void enterEmailAddressAndPassword(String email, String password) {
+        this.emailInput.sendKeys(email);
+        this.passwordInput.sendKeys(password);
     }
 
     /**
-     * Return error messages displayed for user names and passwords.
+     * Return error messages displayed for email addresses and passwords.
      * @return a concatenated string of both user name and password errors, or an empty string if none.
      */
     public String getErrorMessages() {
         String concatenatedErrorMessage = "";
-        if (userNameErrorMessage.isDisplayed()) {concatenatedErrorMessage += userNameErrorMessage.getText();}
+        if (emailInputError.isDisplayed()) {concatenatedErrorMessage += emailInputError.getText();}
         if (!concatenatedErrorMessage.equals("")) {concatenatedErrorMessage += "|";}
-        if (passwordErrorMessage.isDisplayed()) {concatenatedErrorMessage += passwordErrorMessage.getText();}
+        if (passwordInputError.isDisplayed()) {concatenatedErrorMessage += passwordInputError.getText();}
         return concatenatedErrorMessage;
     }
 
     /**
-     * Submit your credentials using the submit button.
+     * Submit your credentials.
      */
     public void submitLogin() {
         submitButton.click();
