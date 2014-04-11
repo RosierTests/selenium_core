7d4bc1d43ade72874ca5769b6c5db795435d9de8
Brian Smith <crazycabo350@gmail.com>
2014-04-11 11:25:29 -0400

    ResultsLogger
    - Removed the need to send in the test case number and elapsed time. Methods now exist to search through the names of all test cases in TestRail to retrieve the test case number dynamically.

--- src/main/utilities/output/ResultsLogger.java
+++ src/main/utilities/output/ResultsLogger.java
@@ -1,8 +1,12 @@
 package utilities.output;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import utilities.ElapsedTime;
 import utilities.output.testrail.APIClient;
+import utilities.output.testrail.APIException;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,42 +18,62 @@ import java.util.Map;
 public class ResultsLogger {
     APIClient client;
     String url, user, password = "";
-    public final String TEST_RUN = "testrun.number";
+    public final String TEST_RUN = "testrun";
 
     public ResultsLogger(String url, String user, String password) {
         this.url = url;
         this.user = user;
         this.password = password;
-    }
 
-    //Add a test result to TestRail via Rest API.
-    public void AddResultsToTestRail(Integer testCase, Integer statusID, Long elapsedTime,
-                                            String comment) throws Exception {
         client = new APIClient(url);
         client.setUser(user);
         client.setPassword(password);
-
-        createNewTestPlan();
-
-        Map data = new HashMap();
-        data.put("status_id", statusID);
-        data.put("comment", comment);
-        data.put("elapsed", getElapsedTime(elapsedTime) + "s");
-
-        String testRunProperty = System.getProperty(TEST_RUN, "0");
-        JSONObject response = (JSONObject) client.sendPost("add_result_for_case/" + testRunProperty + "/" + testCase, data);
-        response.clear();
     }
 
-    //Create a new test plan if not already created.
-    private Boolean createNewTestPlan() {
+    // Add a test result to TestRail via Rest API.
+    public void AddResultsToTestRail(String test, Integer statusID, String comment) throws APIException, IOException {
+        String testRunProperty = System.getProperty(TEST_RUN, "0");
+
+        //createNewTestPlan();
+
+        Map<String, java.io.Serializable> data = new HashMap<>();
+        data.put("status_id", statusID);
+        data.put("comment", comment);
+        data.put("elapsed", ElapsedTime.getElapsedTime() + "s");
+
+        String testCaseNumber = getTestCaseNumber(test);
+        if (!testCaseNumber.equals("none")) {
+            JSONObject response = (JSONObject) client.sendPost(
+                    "add_result_for_case/" + testRunProperty + "/" + testCaseNumber, data);
+            response.clear();
+        }else { throw new APIException("TestRail test case could not be found for \"" + test + "\"."); }
+    }
+
+    // Create a new test plan if not already created.
+    private Boolean createNewTestPlan() throws APIException, IOException {
+        Map<String, java.io.Serializable> data = new HashMap<>();
+        data.put("suite_id", 1);
+        data.put("config_ids", "[1, 2, 3, 4]");
+
+        JSONObject response = (JSONObject) client.sendPost("add_plan/1", data);
+        response.clear();
         return true;
     }
 
-    //Calculate elapsed time in whole Integer seconds by rounding up if necessary.
-    private Integer getElapsedTime(Long elapsedTime) {
-        Integer elapsedTimeInSeconds = Math.round((float) ((int) (elapsedTime / 1000000) * .001));
-        elapsedTimeInSeconds = elapsedTimeInSeconds > 1 ? elapsedTimeInSeconds : 1;
-        return elapsedTimeInSeconds;
+    // Get a count of all test cases in a suite.
+    private JSONArray getTestCases() throws APIException, IOException {
+        return (JSONArray) client.sendGet("get_cases/1&suite_id=1");
+    }
+
+    // Get the test case number for a given test name.
+    private String getTestCaseNumber(String testName) throws APIException, IOException {
+        Integer count = getTestCases().size();
+
+        for (Integer i = 0; i < count; i++) {
+            JSONObject testContents = (JSONObject) getTestCases().get(i);
+            if (testName.equals(testContents.get("title"))) {
+                return testContents.get("id").toString();
+            }
+        }return "none";
     }
 }
