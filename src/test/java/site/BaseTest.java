d1f1fddddb778cabaf4facab39c32469d74ee443
Brian Smith <crazycabo350@gmail.com>
2015-03-31 19:55:05 -0400

    Add base test class

---
+++ src/test/java/site/BaseTest.java
@@ -0,0 +1,58 @@
+package site;
+
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.DriverManager;
+import utilities.output.Screenshot;
+
+/**
+ * Created By: Brian Smith on 10/8/14.
+ * Package: test/java
+ */
+public class BaseTest {
+    public static WebDriver localDriver;
+
+    @Rule
+    public TestName testName = new TestName();
+
+    public static void getScreenShot() {
+        Screenshot screenshot = new Screenshot(localDriver);
+        screenshot.getScreenShot();
+    }
+
+    public static void hideElement(WebElement element) {
+        ((JavascriptExecutor)localDriver).executeScript("arguments[0].style.visibility='hidden'", element);
+        new WebDriverWait(localDriver, 15).until(
+                ExpectedConditions.not(ExpectedConditions.visibilityOf(element)));
+    }
+
+    public static void manualWait(Integer seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void navigateTo(String page) {
+        localDriver.navigate().to("https://cookmedcom:SYjdLT2I2V@beta.cookmedical.com/" + page);
+    }
+
+    public static void openBrowser(String testName) {
+        localDriver = DriverManager.get(testName);
+        if (System.getProperty("remote", "false").equals("false")) {
+            setBrowserDimensions(1024,768);
+        }
+    }
+
+    public static void setBrowserDimensions(Integer w, Integer h) {
+        Dimension browserWindow = new Dimension(w, h);
+        localDriver.manage().window().setSize(browserWindow);
+    }
+}
