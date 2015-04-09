97ece84735cf0de19b5e84f1f3dd6b6d79d9b366
Brian Smith <crazycabo350@gmail.com>
2015-04-09 15:51:37 -0400

    Simplify

---
+++ src/main/site/alpha/Accounts.java
@@ -0,0 +1,31 @@
+package site.alpha;
+
+import org.openqa.selenium.WebDriver;
+import site.alpha.pages.authentication.CreateAccountPage;
+import site.alpha.pages.authentication.LoginPage;
+import utilities.DriverManager;
+
+/**
+ * Created By: bsmith on 3/25/14.
+ * Package: None
+ * Description: This class is used to simply create user accounts.
+ */
+public class Accounts{
+    public static WebDriver localDriver;
+
+    public static void createAccount(String firstName, String lastName, String email, String userName, String password) {
+        localDriver = DriverManager.get("create account");
+
+        // Create an account, verify it, then log out of it.
+        LoginPage loginPage = new LoginPage(localDriver);
+        CreateAccountPage createAccountPage = new CreateAccountPage(localDriver, loginPage);
+        createAccountPage.get();
+        createAccountPage.submitAccountCreationRequest(firstName, lastName, email, userName, password, password);
+
+        if (localDriver.getTitle().equals("TestDriver - Gateway")) {
+            localDriver.navigate().to("http://localhost:3000/logout");
+        }
+
+        localDriver.quit();
+    }
+}
